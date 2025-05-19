f1627658376f9d74fd5208b63f6bd21060218a74
Pedro <30800044+pricci1@users.noreply.github.com>
2025-05-19 12:52:12 -0400

    feat: use apikey in loader

--- apps/astro-blog-demo/src/content.config.ts
+++ apps/astro-blog-demo/src/content.config.ts
@@ -3,7 +3,8 @@ import { ghDiscussionsLoader } from "@repo/gh-discussions-astro-loader";
 
 const blog = defineCollection({
   loader: ghDiscussionsLoader({
-    repoUrl: "https://github.com/username/repo",
+    repoUrl: "https://github.com/pricci1/discussions-playground",
+    apiKey: import.meta.env.GH_API_KEY,
   }),
 });
 
